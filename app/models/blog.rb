4fb39fec87e20d168a0a29167c4bf95842da4809
Vlada992 <vladanng@gmail.com>
2019-08-16 11:15:47 +0200

    implemented validation for blog/skills/portfolio items

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,10 @@ class Blog < ApplicationRecord
     enum status: {draft: 0, published: 1}
     extend FriendlyId
     friendly_id :title, use: :slugged
+
+    validates_presence_of :title, :body #not gonna alow db/instance creation of blog class without title or body key
 end
+
+
+#enum for toggling draft or published
+#FriendlyId for mapping id to names
