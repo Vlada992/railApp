dcc7fae8cba9fed19a2f9b8774f4efd44a92df12
Vlada992 <vladanng@gmail.com>
2019-08-16 12:20:48 +0200

    integrated database relationship between topics - blog posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,6 +4,8 @@ class Blog < ApplicationRecord
     friendly_id :title, use: :slugged
 
     validates_presence_of :title, :body #not gonna alow db/instance creation of blog class without title or body key
+
+    belongs_to :topic          #this model(Blog class) belonfs to Topic model(Topic class)
 end
 
 
