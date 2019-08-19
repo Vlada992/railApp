45ecac32afd049a8643830b96fdd15cc16cbc71b
Vlada992 <vladanng@gmail.com>
2019-08-19 15:34:37 +0200

    new parameters for user registration and editin account

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -1,3 +1,11 @@
 class Technology < ApplicationRecord
   belongs_to :portfolio
 end
+
+
+
+#so, class Technology, created from terminal like: rails g model Tech rails g model Tech name:string portfolio:references
+
+
+#so we created Technology model, rails g model Technology name:string portfolio:references
+# technology_attributes are prop on Portfolio.create() method
