dcb0d9bc30f7b08c2cb1c2a9b89d018540d72ff1
Vlada992 <vladanng@gmail.com>
2019-08-19 17:07:47 +0200

    completed authentication with virtual attributes taking from user input

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -3,16 +3,13 @@
 
     included do
         before_action :configure_permitted_parameters, if: :devise_controller?
-
     end
-
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit( :sign_up, keys: [:name] ) #just pass newone you want to permit, add in list
         devise_parameter_sanitizer.permit( :account_update, keys: [:name] )
     end
 
-   
 
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  validates_presence_of :name         #validate that name exist, user typed id
+
+
+  def first_name
+    self.name.split.first   
+  end
+
+  def last_name
+    self.name.split.last  
+
+  end
+
 end
+
+
+
+
+# first/lat _name methods, will split and slice first and last name fro user input(expects to be first and last name) and hodl it here
+ #this is just Class object
