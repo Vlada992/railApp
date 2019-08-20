3b1fa37cc49cd80d535f0f01e1a0f50cfb86a2da
Vlada992 <vladanng@gmail.com>
2019-08-20 10:31:10 +0200

    refactoring ApplicationController to include Concern for source feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,7 @@
 class ApplicationController < ActionController::Base
 
     include DeviseWhitelist
-
-    before_action :set_source #EXECUTE this method before everything
-
-    def set_source
-        session[:source] = params[:q] if params[:q] # WE STORE QUERY VALUE IN SESSION NEW PROP
-    end
-
+    include SetSource #method for setting query source string in session prop
 
 end
 
