8f44b84027505d9d9f354b67c423790c53044a90
Vlada992 <vladanng@gmail.com>
2019-08-20 12:23:27 +0200

    added guest_user for curren_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
 
-    include DeviseWhitelist
+    include DeviseWhitelist #including this module
     include SetSource #method for setting query source string in session prop
-
+    include CurrentUserConcern
 end
 
 
 #set_source method, store in session[:source] propery whatever you get from user on querry
+#including 2 modules for now here in this class, in this object class
+
+# super || , if super not returning nil, add that, otherwise ad OpenStruct hash
