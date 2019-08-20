517bbad5b0775b8f8eae5fef115b19e6eabf585e
Vlada992 <vladanng@gmail.com>
2019-08-20 10:17:26 +0200

    added session tracker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
 
     include DeviseWhitelist
 
+    before_action :set_source #EXECUTE this method before everything
+
+    def set_source
+        session[:source] = params[:q] if params[:q] # WE STORE QUERY VALUE IN SESSION NEW PROP
+    end
+
+
 end
+
+
+#set_source method, store in session[:source] propery whatever you get from user on querry
