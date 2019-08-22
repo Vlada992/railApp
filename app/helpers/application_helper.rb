be71aa27e7001e5262a5cbfe804ed5530f97a937
Vlada992 <vladanng@gmail.com>
2019-08-22 15:42:53 +0200

    bug with login helper method, fix, by setting default arguments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   #it will enter to look for all the methods in thsi global application helper
 
-  def login_helper(style)
+  def login_helper(style= "")         #default arguments :)
     if current_user.is_a?(GuestUser)
       (link_to "Register", new_user_registration_path, class: style) +
       " ".html_safe +
